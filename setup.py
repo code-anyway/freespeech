acc82b73e598b8151596d8aed3f43d18f1f6a27a
astaff <astaff@users.noreply.github.com>
2022-03-10 20:53:23 +0000

    Initial structure

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup
+import os
+
+VERSION = "0.1"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="freespeech",
+    description="null",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Artyom Astafurov",
+    url="https://github.com/astaff/freespeech",
+    project_urls={
+        "Issues": "https://github.com/astaff/freespeech/issues",
+        "CI": "https://github.com/astaff/freespeech/actions",
+        "Changelog": "https://github.com/astaff/freespeech/releases",
+    },
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["freespeech"],
+    entry_points="""
+        [console_scripts]
+        freespeech=freespeech.cli:cli
+    """,
+    install_requires=["click"],
+    extras_require={
+        "test": ["pytest"]
+    },
+    python_requires=">=3.7",
+)
