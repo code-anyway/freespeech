433f10b79015db674661a84425763383899bd1d0
Artyom Astafurov <artyom.astafurov@gmail.com>
2022-03-15 15:39:12 -0400

    add initial container spec

--- workflows/ingest_synthesize_mix.sh
+++ workflows/ingest_synthesize_mix.sh
@@ -3,27 +3,28 @@
 # Example Usage:
 #
 # ingest_synthesize_mix \
-    # https://youtu.be/wl26_J700sA \
-    # https://docs.google.com/document/d/1atdjoPsrk8KVPyspZpDyIqoC_n0usVNWmKGnMYXnNLA/edit \
-    # ru-RU \
-    # ru-RU-Wavenet-D
+#   https://youtu.be/wl26_J700sA \
+#   https://docs.google.com/document/d/1atdjoPsrk8KVPyspZpDyIqoC_n0usVNWmKGnMYXnNLA/edit \
+#   ru-RU \
+#   ru-RU-Wavenet-D \
+#   /root/data
 #
 # ingest_synthesize_mix \
 #     https://youtu.be/wl26_J700sA \
 #     https://docs.google.com/document/d/1atdjoPsrk8KVPyspZpDyIqoC_n0usVNWmKGnMYXnNLA/edit \
 #     en-EN \
-#     en-EN-Wavenet-I
+#     en-EN-Wavenet-I \
+#     /root/data
 
-
-freespeech ingest --url=$1 --output_dir=output/
+freespeech ingest --url=$1 --output_dir=$5
 freespeech synthesize \
     --text_file=$2 \
-    --output=output/speech-$3-$4.wav \
+    --output=$5/speech-$3-$4.wav \
     --language=$3 \
     --voice=$4 \
-    --duration=$(freespeech probe output/video.mp4 | jq -r .format.duration)
+    --duration=$(freespeech probe $5/video.mp4 | jq -r .format.duration)
 freespeech mix \
-    --video=output/video.mp4 \
-    --output=output/video-$3-$4.mp4 \
+    --video=$5/video.mp4 \
+    --output=$5/video-$3-$4.mp4 \
     --weights=1,10 -- \
-    output/audio.webm output/speech-$3-$4.wav
+    $5/audio.webm $5/speech-$3-$4.wav
