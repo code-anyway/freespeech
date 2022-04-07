70b53c13a79d8091c1496a184a377d45894baf8f
Artyom Astafurov <artyom.astafurov@gmail.com>
2022-04-07 15:47:08 -0400

    fall back to HTTP API to determine project id

--- freespeech/env.py
+++ freespeech/env.py
@@ -1,14 +1,38 @@
 import json
 import os
+import requests
+import logging
 
 
-def get_service_account_file() -> str:
-    return os.environ["GOOGLE_APPLICATION_CREDENTIALS"]
+logger = logging.getLogger(__name__)
+
+PROJECT_ID_URL = \
+    "http://metadata.google.internal/computeMetadata/v1/project/project-id"
+
+
+def get_service_account_file() -> str | None:
+    path = os.environ.get("GOOGLE_APPLICATION_CREDENTIALS", None)
+
+    if not path:
+        logger.info("GOOGLE_APPLICATION_CREDENTIALS is not set")
+
+    return path
 
 
 def get_project_id() -> str:
-    with open(get_service_account_file()) as fd:
-        return json.load(fd)["project_id"]
+    file = get_service_account_file()
+
+    if not file:
+        logger.info(
+            f"Service account file is not set. Will use {PROJECT_ID_URL}")
+        response = requests.get(
+            url=PROJECT_ID_URL,
+            headers={"Metadata-Flavor": "Google"}
+        )
+        return response.text
+    else:
+        with open(file) as fd:
+            return json.load(fd)["project_id"]
 
 
 def get_storage_url() -> str:
