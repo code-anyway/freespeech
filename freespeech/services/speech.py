a32fb5727919168fef45918707dcda71f1a4d8a3
Artyom Astafurov <artyom.astafurov@gmail.com>
2022-04-15 19:20:29 -0400

    fix linting error; make flake8 play nicely with black

--- freespeech/services/speech.py
+++ freespeech/services/speech.py
@@ -4,5 +4,6 @@ routes = web.RouteTableDef()
 
 # Service: speech
 # POST /media/{id}/{lang}/transcribe
-# Async. Launches a transcription job that will update media's transcript for a given language
+# Async. Launches a transcription job that will
+# update media's transcript for a given language
 # TODO: return a job id, maintain a jon queue, don't allow duplicates.
