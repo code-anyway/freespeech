3d20895ace954325cdbef465bfb942b1fe85c341
Artyom Astafurov <artyom.astafurov@gmail.com>
2022-06-06 14:12:51 -0400

    add media.cut function; update pytube to the latest patch

--- setup.py
+++ setup.py
@@ -42,8 +42,8 @@ setup(
         "click",
         "ffmpeg-python",
         # TODO (astaff): update/remove after https://github.com/pytube/pytube/pull/1282
-        # is merged/released
-        "pytube @ git+https://github.com/kinshuk-h/pytube.git@72075dddb2153bde89a8de9eb8def91d41da3655#egg=pytube-12.0.1",  # noqa E501
+        # is merged/released,
+        "pytube @ git+https://github.com/brilliant-ember/pytube.git@a3c96b92a517d7e2978a45112cbf11993271c010#egg=pytube-12.0.1",  # noqa E501
         "google-cloud-texttospeech",
         "google-cloud-translate",
         "google-cloud-storage",
