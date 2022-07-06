ed4445e3cee72bd463d02bd312ce0c54df60ea8b
Artyom Astafurov <artyom.astafurov@gmail.com>
2022-07-06 02:28:18 -0400

    add type ignore

--- freespeech/lib/language.py
+++ freespeech/lib/language.py
@@ -99,9 +99,9 @@ async def intent(
     project_name = "chat-bot"
     deployment_name = "prod"
 
-    # todo (astaff 07/06/2022) confirm that the line below
-    # resolves https://github.com/astaff/freespeech/runs/7148219569
-    client = ConversationAnalysisClient(url, AzureKeyCredential(token))
+    # todo (astaff 07/06/2022) remove type ignore after fixing
+    # https://github.com/astaff/freespeech/issues/76
+    client = ConversationAnalysisClient(url, AzureKeyCredential(token))  # type: ignore
     async with client:
         result = await client.analyze_conversation(
             task={
