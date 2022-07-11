22e2f6f1af867ba5019b0dc7b14ad32c9fb42de4
Alex Cherednichenko <lexaux@gmail.com>
2022-07-11 19:56:42 +0300

    Remove redundant file

--- freespeech/logging.py
+++
@@ -1,8 +0,0 @@
-import google.cloud.logging
-from google.cloud.logging.handlers import CloudLoggingHandler
-
-
-class GoogleCloudLoggingHandler(CloudLoggingHandler):
-    def __init__(self):
-        client = google.cloud.logging.Client()
-        super().__init__(client=client)
