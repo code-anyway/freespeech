0d672bf34201d7bb3903b9b896763f8f57454587
Alex Cherednichenko <lexaux@gmail.com>
2022-07-21 15:06:58 +0300

    Make retry decorator test isolated
    Test now does not depend on Telegram facilities and needs no key to run.

--- tests/api/test_telegram.py
+++ tests/api/test_telegram.py
@@ -1,7 +1,9 @@
 import asyncio
 import logging
+from typing import Dict
 
 import pytest
+from aiogram.utils.exceptions import RetryAfter
 
 import freespeech.api.telegram as telegram_api
 
@@ -9,12 +11,20 @@ logger = logging.getLogger(__name__)
 
 
 @pytest.mark.asyncio
-async def test_telegram_webhook_ratelimit():
-    async def _install_webhook(message: str):
-        logger.info(f"Executing {message}")
-        assert isinstance(message, str)
-        await telegram_api.bot.set_webhook("https://localhost.com")
+async def test_ratelimit_retry_decorator():
+    attempt_count: Dict[str, int] = {}
 
-    call_with_retries = telegram_api.handle_ratelimit(_install_webhook)
+    async def _install_webhook_mock(message: str):
+        current_attempts = attempt_count.get(message, 0) + 1
+        attempt_count[message] = current_attempts
+        logger.info(f"Executing {message}, attempt {current_attempts}")
+
+        if current_attempts > 3:
+            # assume success after third attempt
+            return
+        else:
+            raise RetryAfter(0.1)
+
+    call_with_retries = telegram_api.handle_ratelimit(_install_webhook_mock)
 
     await asyncio.gather(*[call_with_retries(f"Sample {i}") for i in range(1, 4)])
