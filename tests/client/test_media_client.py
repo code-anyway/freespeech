a17c6612b5327888995f5e4b10a3197588f38ef4
Artyom Astafurov <artyom.astafurov@gmail.com>
2022-07-26 15:41:22 -0400

    allow storing env variables in .env file; uncomment media client test

--- tests/client/test_media_client.py
+++ tests/client/test_media_client.py
@@ -18,14 +18,14 @@ async def client(aiohttp_client) -> Generator[AiohttpClient, None, None]:
     return await aiohttp_client(app, timeout=aiohttp.ClientTimeout(total=10000))
 
 
-@pytest.mark.skip(reason="Long test, enable to call ingest")
+# @pytest.mark.skip(reason="Long test, enable to call ingest")
 @pytest.mark.asyncio
 async def test_jsononly_ingest(client):
     import time
 
     start_time = time.time()
 
-    # resp = await media_client.ingest(
-    #     "https://www.youtube.com/watch?v=Gm4qV0wX8f0", session=client
-    # )
+    resp = await media_client.ingest(
+        "https://www.youtube.com/watch?v=Gm4qV0wX8f0", session=client
+    )
     print("--- %s seconds ---" % (time.time() - start_time))
