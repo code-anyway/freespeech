d19f9f43f476dd06369384e6cdae96c177b58fd6
Artyom Astafurov <artyom.astafurov@gmail.com>
2022-08-05 13:45:07 -0400

    update dispatch_deadline as per examples

--- freespeech/lib/tasks/cloud_tasks.py
+++ freespeech/lib/tasks/cloud_tasks.py
@@ -1,8 +1,10 @@
+import logging
 import uuid
 from typing import Dict
 
 from google import api_core
 from google.cloud import tasks_v2
+from google.protobuf import duration_pb2
 
 from freespeech import env
 from freespeech.lib.tasks import results
@@ -10,6 +12,9 @@ from freespeech.types import Task
 
 LOCATION = "us-central1"
 QUEUE_NAME = "main"
+TASK_TIMEOUT_SEC = 30 * 60
+
+logger = logging.getLogger(__name__)
 
 
 def get_task_path(client, task_id):
@@ -37,9 +42,14 @@ async def schedule(method: str, url: str, headers: Dict, payload: bytes) -> Task
             },
             "body": payload,
         },
-        # more here: https://github.com/googleapis/python-tasks/issues/93
-        "dispatch_deadline": "1800s",
     }
+
+    # Add dispatch deadline for requests sent to the worker.
+    # more here: https://github.com/googleapis/python-tasks/issues/93
+    duration = duration_pb2.Duration()
+    dispatch_deadline = duration.FromSeconds(TASK_TIMEOUT_SEC)  # type: ignore
+    logger.debug(f"Setting dispatch_deadline to {dispatch_deadline}")
+    task["dispatch_deadline"] = dispatch_deadline
 
     client.create_task(request={"parent": parent, "task": task})
 
