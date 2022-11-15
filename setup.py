fa993f234e8b722f14e628d9354ad78feb64f968
Artyom Astafurov <artyom.astafurov@gmail.com>
2022-11-15 13:14:08 -0500

    add librosa, libsndfile, implement lib/audio and lib/tts

--- setup.py
+++ setup.py
@@ -46,6 +46,7 @@ setup(
         "deepgram-sdk",
         "fastapi[all]",
         "ffmpeg-python",
+        "librosa",
         # TODO (astaff): update/remove after https://github.com/pytube/pytube/pull/1282
         # is merged/released,
         "librosa",
@@ -65,6 +66,7 @@ setup(
         "pydantic",
         "pytz",
         "requests",
+        "soundfile",
         "spacy",
         *[
             f"{lang}_core_web_sm @ https://github.com/explosion/spacy-models/releases/download/{lang}_core_web_sm-3.4.0/{lang}_core_web_sm-3.4.0.tar.gz"
