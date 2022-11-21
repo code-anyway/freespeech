ebb18bb9a0d871a7c44fb4e934b5c22b86b33cd5
Artyom Astafurov <artyom.astafurov@gmail.com>
2022-11-21 16:19:10 -0500

    make process/thread pool local to functions, remove extra probe() in media

--- freespeech/lib/concurrency.py
+++ freespeech/lib/concurrency.py
@@ -2,15 +2,14 @@ import asyncio
 import concurrent.futures
 from typing import Any, Callable
 
-THREAD_POOL = concurrent.futures.ThreadPoolExecutor()
-PROCESS_POOL = concurrent.futures.ProcessPoolExecutor()
-
 
 async def run_in_thread_pool(func: Callable[..., Any], *args: Any) -> Any:
-    loop = asyncio.get_running_loop()
-    return await loop.run_in_executor(THREAD_POOL, func, *args)
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(pool, func, *args)
 
 
 async def run_in_process_pool(func: Callable[..., Any], *args: Any) -> Any:
-    loop = asyncio.get_running_loop()
-    return await loop.run_in_executor(PROCESS_POOL, func, *args)
+    with concurrent.futures.ThreadPoolExecutor() as pool:
+        loop = asyncio.get_running_loop()
+        return await loop.run_in_executor(pool, func, *args)
