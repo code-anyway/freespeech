707d72402577fa48d16c446494a6704c9c9b2d02
Artyom Astafurov <artyom.astafurov@gmail.com>
2023-03-01 15:59:16 -0500

    swap pytube dependency for yt-dlp

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ setup(
         # TODO (astaff): update/remove after https://github.com/pytube/pytube/pull/1282
         # is merged/released,
         "librosa",
-        "pytube @ git+https://github.com/brilliant-ember/pytube.git@a3c96b92a517d7e2978a45112cbf11993271c010#egg=pytube-12.0.1",  # noqa E501
+        "yt-dlp",
         "google-cloud-texttospeech",
         "google-cloud-translate",
         "google-cloud-storage",
