2ced2033686be5576208f290846fc5f6591ca084
Artyom Astafurov <artyom.astafurov@gmail.com>
2023-05-21 18:36:25 +0200

    checkpoint

--- freespeech/lib/tts.py
+++ freespeech/lib/tts.py
@@ -3,12 +3,13 @@ from itertools import zip_longest
 from pathlib import Path
 from uuid import uuid4
 
-from pydub import AudioSegment
+from pydub import AudioSegment, silence
 
 from freespeech.lib import audio, speech
 from freespeech.types import Event, Language, Voice
 
 PAUSE_INCREMENT_MS = 100.0
+FRAME_RATE = 44100
 
 
 async def synthesize_phrase(
@@ -26,30 +27,59 @@ async def synthesize_phrase(
     return AudioSegment.from_file(audio.strip(clip))
 
 
+def detect_speech_and_pauses(seg: AudioSegment) -> tuple[list[AudioSegment], list[int]]:
+    non_silent = silence.detect_nonsilent(seg, min_silence_len=25, silence_thresh=-50)
+    print(non_silent)
+    speech_segments = []
+    pauses = []
+
+    if not non_silent:
+        return [], [len(seg)]
+
+    if non_silent:
+        pauses.append(non_silent[0][0])
+
+    for i, (start, end) in enumerate(non_silent):
+        speech_segments.append(seg[start:end])
+        if i < len(non_silent) - 1:
+            pauses.append(non_silent[i + 1][0] - end)
+        else:
+            pauses.append(len(seg) - end)
+
+    return speech_segments, pauses
+
+
 async def synthesize_text(
     text: str, duration_ms: int | None, voice: Voice, lang: Language, output_dir: Path
 ) -> tuple[AudioSegment, int]:
     phrases = re.split(r"(#+((\d+(\.\d+)?)#)?)", text)
-    audio_segments = []
 
     # replace & with and
     text = text.replace("&", "and")
 
     # Always start with a pause
-    pauses = [PAUSE_INCREMENT_MS]
+    res = AudioSegment.silent(duration=int(PAUSE_INCREMENT_MS), frame_rate=FRAME_RATE)
 
     for phrase, pause, duration in zip_longest(
         phrases[0::5], phrases[1::5], phrases[3:5], fillvalue=""
     ):
         if duration is None or duration == "":
             pause_ms = len(pause) * PAUSE_INCREMENT_MS
-            pauses.append(pause_ms)
         else:
-            pauses.append(float(duration) * 1000)
-        audio_segments.append(await synthesize_phrase(phrase, voice, lang, output_dir))
+            pause_ms = float(duration) * 1000
 
-    # Always end with a pause
-    pauses.append(PAUSE_INCREMENT_MS)
+        res += await synthesize_phrase(phrase, voice, lang, output_dir)
+        res += AudioSegment.silent(duration=int(pause_ms), frame_rate=FRAME_RATE)
+
+    output_file = str(Path(output_dir) / f"{uuid4()}.wav")
+    fd = res.export(output_file, format="wav")
+    fd.close()  # type: ignore
+
+    audio_segments, pauses = detect_speech_and_pauses(
+        AudioSegment.from_file(output_file)
+    )
+
+    print(pauses)
 
     total_speech_ms = sum(map(len, audio_segments))
     total_pause_ms = sum(pauses)
@@ -58,25 +88,23 @@ async def synthesize_text(
         duration_ms = int(total_speech_ms + total_pause_ms)
 
     pause_scale = (duration_ms - total_speech_ms) / total_pause_ms
-    pauses = [pause * pause_scale if pause_scale >= 0 else 0 for pause in pauses]
+    pauses = [int(pause * pause_scale) if pause_scale >= 0 else 0 for pause in pauses]
+
+    print(pauses)
 
     frame_rate = (
         audio_segments[0].frame_rate
         if audio_segments and audio_segments[0].frame_rate
-        else 44100
+        else FRAME_RATE
     )
 
     res = AudioSegment.empty()
 
-    lead_pause = pauses.pop(0)
-    if lead_pause > 0:
-        res += AudioSegment.silent(duration=int(lead_pause), frame_rate=frame_rate)
-
     for audio_segment, pause in zip_longest(audio_segments, pauses, fillvalue=None):
-        if audio_segment is not None:
-            res += audio_segment
         if pause is not None and pause > 0:
             res += AudioSegment.silent(duration=int(pause), frame_rate=frame_rate)
+        if audio_segment is not None:
+            res += audio_segment
 
     return res, len(res) - duration_ms
 
@@ -87,7 +115,10 @@ async def synthesize(
     output_dir: str,
 ) -> Path:
     carry_over = 0
+
     res = AudioSegment.empty()
+    if events and events[0].time_ms is not None and events[0].time_ms > 0:
+        res += AudioSegment.silent(duration=events[0].time_ms, frame_rate=FRAME_RATE)
 
     for i, event in enumerate(events):
         duration_ms = None
