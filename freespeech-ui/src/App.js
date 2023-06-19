4b6616f8f6c7c63c3ed9b34662ff59852f1e057e
Dorothy <dorothychauu@gmail.com>
2023-06-19 09:25:29 -0400

    first commit

---
+++ freespeech-ui/src/App.js
@@ -0,0 +1,24 @@
+import logo from './freespeechlogo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+        </p>
+        <a
+          className="App-link"
+          href="https://freespeechnow.ai/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Welcome to FreeSpeech
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
