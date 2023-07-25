2c377c0a986824b913600810dccfa87ba3d73ade
axelKeizoStahl <axelkeizo@gmail.com>
2023-07-25 16:46:36 -0400

    optimized dub_cache_test

--- tests/lib/test_speech.py
+++ tests/lib/test_speech.py
@@ -840,7 +840,7 @@ async def test_dub_cache(tmp_path) -> None:  # noqa E501
     cache_dir = os.path.join(os.path.dirname(__file__), "../../cache")
     testing_text = "Elephant banana clock waterfall zebra spaceship rainbow apple mountain guitar moon cheese pizza starfish unicorn sunflower jellyfish spaceship popcorn monkey watermelon dinosaur spaceship robot cookie ocean pencil catfish balloon kangaroo dragon peanut jelly shirt basketball rocket turtle pineapple rainbow giraffe spaceship caterpillar rainbow coffee lamp potato octopus spaceship rocket moon kangaroo donut lighthouse rainbow book skateboard spaceship tree frog ice cream strawberry pencil rainbow turtle volcano dragon telescope spaceship popcorn mushroom spaceship butterfly moon rainbow guitar unicorn spaceship tomato spaceship dragon octopus rainbow elephant starfish penguin spaceship pineapple cheese cupcake rainbow spaceship robot book rainbow spaceship spaceship spaceship."  # noqa E501
     non_cached_function_time = 0.0
-    for i in range(2):
+    for i in range(10):
         start_time = time.time()
         output, voice = await speech.synthesize_text(
             text=testing_text,
@@ -851,31 +851,16 @@ async def test_dub_cache(tmp_path) -> None:  # noqa E501
         )
         end_time = time.time()
         non_cached_function_time += end_time - start_time
-        # Here the remove files can be put to the top and then we can delete the extra call on the bottom
-        os.remove(
-            f"{cache_dir}/{hash.obj((testing_text, None, Voice(character='Alan', pitch=0.0, speech_rate=1.0), 'en-US'))}.wav"  # noqa E501
-        )
-        os.remove(
-            f"{cache_dir}/{hash.obj((testing_text, None, Voice(character='Alan', pitch=0.0, speech_rate=1.0), 'en-US'))}-voice.json"  # noqa E501
-        )
-    non_cached_function_time /= 10
-    output, voice = await speech.synthesize_text(
-        text=testing_text,
-        duration_ms=None,
-        voice=Voice(character="Alan"),
-        lang="en-US",
-        output_dir=tmp_path,
-    )
-    # the os.remove files in the loop should do the same thing as this and therefore check if the cache is made
-    try:
-        os.remove(
-            f"{cache_dir}/{hash.obj((testing_text, None, Voice(character='Alan', pitch=0.0, speech_rate=1.0), 'en-S'))}.wav"  # noqa E500
-        )
-        os.remove(
-            f"{cache_dir}/{hash.obj((testing_text, None, Voice(character='Alan', pitch=0.0, speech_rate=1.0), 'en-US'))}-voice.json"
-        )
-    except FileNotFoundError as file_not_found:
-        pytest.fail(f"Unexpected Error: {file_not_found}")
+
+        try:
+            os.remove(
+                f"{cache_dir}/{hash.obj((testing_text, None, Voice(character='Alan', pitch=0.0, speech_rate=1.0), 'en-US'))}.wav"  # noqa E500
+            )
+            os.remove(
+                f"{cache_dir}/{hash.obj((testing_text, None, Voice(character='Alan', pitch=0.0, speech_rate=1.0), 'en-US'))}-voice.json"  # noqa E500
+            )
+        except FileNotFoundError as file_not_found:
+            pytest.fail(f"Unexpected Error: {file_not_found}")
 
     cached_function_time = 0.0
     for i in range(10):
